f7a5e7fdcfbc34402368884f7fb61123febe548c
Hirochika Asai <asai@scyphus.co.jp>
2014-10-04 15:03:53 +0900

    Release v0.1.0

---
+++ truncate/main.c
@@ -0,0 +1,18 @@
+/*_
+ * Copyright (c) 2014 Hirochika Asai.
+ * All rights reserved.
+ *
+ * Authors:
+ *      Hirochika Asai  <asai@jar.jp>
+ */
+
+
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: sw=4 ts=4 fdm=marker
+ * vim<600: sw=4 ts=4
+ */
